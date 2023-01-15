54fa1293a0694e47f50c50947e708330ce42f378
Shopping <xpzheng6@163.com>
2023-01-15 13:42:06 +0800

    chore(base): update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   extends: [
     '@commitlint/config-conventional'
-  ]
+  ],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-enum': [2, 'always', ['base', 'server', 'web', 'docs']]
+  },
 }
