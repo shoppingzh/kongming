d186e67bdbd31aaf376e8ccd704624a27384aab8
Shopping <xpzheng6@163.com>
2023-03-14 21:32:25 +0800

    feat(server): mysql host config -> env

--- packages/server/config/config.default.ts
+++ packages/server/config/config.default.ts
@@ -17,7 +17,7 @@ export default (appInfo: EggAppInfo) => {
 
   config.typeorm = {
     type: 'mysql',
-    host: 'localhost',
+    host: process.env.MYSQL_HOST || 'localhost',
     port: 3306,
     username: 'root',
     password: 'xpzheng',
